e39555bab6aecc23efa00f0904e7625fbe9cbdef
sepehr <sepehrhashtroudi@gmail.com>
2021-02-11 21:46:15 -0700

    added tests

--- test/org/jfree/data/test/DataUtilitiesTest.java
+++ test/org/jfree/data/test/DataUtilitiesTest.java
@@ -12,12 +12,19 @@ public class DataUtilitiesTest {
     }
     @Before
     public void setUp() throws Exception {
+
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateNumberArrayNullInput(){
+        DataUtilities.createNumberArray(null);
+    }
+
     @Test
     public void testCreateNumberArray() {
         double[] list = new double[10];
         for (int i = 0; i < 10; i ++){
-            list[i] = i  *1.1;
+            list[i] = i  * 1.1;
         }
         Number[] generated_list = DataUtilities.createNumberArray(list);
         assertEquals(list.length, 10);
@@ -26,6 +33,32 @@ public class DataUtilitiesTest {
             assertEquals(list[i], generated_list[i]);
         }
     }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateNumberArray2dNullInput(){
+        DataUtilities.createNumberArray2D(null);
+    }
+
+    @Test
+    public void testCreateNumberArray2d() {
+        double[][] list = new double[10][10];
+        for (int i = 0; i < 10; i ++){
+            for(int j=0 ; j<10 ;j++)
+            {
+                list[i][j] = i  * 1.1 + j;
+            }
+        }
+        Number[][] generated_list = DataUtilities.createNumberArray2D(list);
+        assertEquals(list.length, 10);
+        assertEquals(generated_list.length, 10);
+        for (int i = 0; i < 10; i++){
+            for(int j = 0 ; j < 10; j++){
+                assertEquals(list[i][j], generated_list[i][j]);
+            }
+        }
+    }
+
     @After
     public void tearDown() throws Exception {
     }
