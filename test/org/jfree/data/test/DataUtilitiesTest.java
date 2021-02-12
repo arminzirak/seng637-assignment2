a4eab29c8d33ea0ec06b7e4ce9a1bb6eb4f3e65c
sepehr <sepehrhashtroudi@gmail.com>
2021-02-11 22:59:38 -0700

    two d corner cases added + comments are added

--- test/org/jfree/data/test/DataUtilitiesTest.java
+++ test/org/jfree/data/test/DataUtilitiesTest.java
@@ -55,8 +55,75 @@ public class DataUtilitiesTest {
         DataUtilities.createNumberArray2D(null);
     }
 
-    @Test
+    @Test // testing the equivalence of 2 array values
     public void testCreateNumberArray2d() {
+        double[][] list = new double[10][10];
+        for (int i = 0; i < 10; i ++){
+            for(int j=0 ; j<10 ;j++)
+            {
+                list[i][j] = i  * 1.1 + j;
+            }
+        }
+        Number[][] generated_list = DataUtilities.createNumberArray2D(list);
+        assertEquals(list.length, 10);
+        assertEquals(generated_list.length, 10);
+        for (int i = 0; i < 10; i++){
+            for(int j = 0 ; j < 10; j++){
+                assertEquals(list[i][j], generated_list[i][j]);
+            }
+        }
+    }
+
+    @Test // testing the array with One Row
+    public void testCreateNumberArray2dWithOneRow() {
+        double[][] list = new double[1][10];
+        for(int i=0 ; i<10 ;i++)
+        {
+            list[0][i] = i  * 1.1;
+        }
+        Number[][] generated_list = DataUtilities.createNumberArray2D(list);
+        assertEquals(generated_list.length, 1);
+        assertEquals(generated_list[0].length, 10);
+        for(int i=0 ; i<10 ;i++)
+        {
+            assertEquals(list[0][i], generated_list[0][i]);
+        }
+    }
+
+    @Test // testing the array with One Column
+    public void testCreateNumberArray2dWithOneColumn() {
+        double[][] list = new double[10][1];
+        for(int i=0 ; i<10 ;i++)
+        {
+            list[i][0] = i  * 1.1;
+        }
+        Number[][] generated_list = DataUtilities.createNumberArray2D(list);
+        assertEquals(generated_list.length, 10);
+        assertEquals(generated_list[0].length, 1);
+        for(int i=0 ; i<10 ;i++)
+        {
+            assertEquals(list[i][0], generated_list[i][0]);
+        }
+    }
+
+    @Test // testing the array with Zero Column
+    public void testCreateNumberArray2dWithZeroColumn() {
+        double[][] list = new double[10][1];
+        for(int i=0 ; i<10 ;i++)
+        {
+            list[i][0] = i  * 1.1;
+        }
+        Number[][] generated_list = DataUtilities.createNumberArray2D(list);
+        assertEquals(generated_list.length, 10);
+        assertEquals(generated_list[0].length, 1);
+        for(int i=0 ; i<10 ;i++)
+        {
+            assertEquals(list[i][0], generated_list[i][0]);
+        }
+    }
+
+    @Test // testing the array with size Zero
+    public void testCreateNumberArray2dWithSizeZero() {
         double[][] list = new double[10][10];
         for (int i = 0; i < 10; i ++){
             for(int j=0 ; j<10 ;j++)
